73e49878df2c80aa954dadc87ad8223926ae9b2f
Ariana Slaviček <aslavicek21@foi.hr>
2023-05-09 12:13:03 +0200

    evaluation

---
+++ EvaluationManager_G08/EvaluationManager_G08/FRMlogin.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EvaluationManager_G08 {
+    public partial class FRMlogin : Form {
+        public FRMlogin() {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e) {
+
+        }
+
+        private void txtUserName_TextChanged(object sender, EventArgs e) {
+
+        }
+
+        private void txtPassword_TextChanged(object sender, EventArgs e) {
+
+        }
+
+        private void FRMlogin_Load(object sender, EventArgs e) {
+
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e) {
+            
+        }
+    }
+}
